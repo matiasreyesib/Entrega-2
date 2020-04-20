6bd9f07d86a1ca0016311958ecd33fbba5990cdb
SebastianGonzalezRP <38021173+SebastianGonzalezRP@users.noreply.github.com>
2020-04-19 21:54:11 -0400

    Correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_one :profile
   belongs_to :member
   belongs_to :participation
-  has_and_belong_to_many :organizations, trough: :member
+  has_and_belongs_to_many :organizations, trough: :member
   has_many :messages
-  has_many :events, trough: :participation
+  has_and_belongs_to_many  :events, trough: :participation
   has_many :comments
   has_many :comment_replies
   has_many :date_votes
