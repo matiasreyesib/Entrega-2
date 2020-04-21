b161931470f180efad5053b856b8d6da66cfb65b
matiasreyesib <mireyes@miuandes.cl>
2020-04-20 22:31:51 -0400

    commit 8

--- db/migrate/20200419070103_create_members.rb
+++ db/migrate/20200419070103_create_members.rb
@@ -1,7 +1,7 @@
 class CreateMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :members do |t|
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.references :organization, null: false, foreign_key: true
       t.string :rol
 
